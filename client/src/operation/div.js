d06b7c0bb2f595bfb70598725ea4233ef1a082f5
Brayan-Andreys <109758319+Andreys-Gp@users.noreply.github.com>
2024-06-12 19:05:59 -0500

    reload
    reload

---
+++ client/src/operation/div.js
@@ -0,0 +1,45 @@
+
+
+import React from 'react';
+
+const Div = (res) => {
+    return (
+        form.addEventListener("submit", e => {
+            let entrada1=document.getElementById("primaria").value;
+            let entrada2=document.getElementById("segunda").value;
+            let entrada3=document.getElementById("tercera").value;
+            let elementoResultado=document.getElementById("resultado")
+                e.preventDefault();
+                
+            
+            
+                
+                            let cadena1=String(entrada1);
+                            let cadena2=String(entrada2);
+                            let cadena3=String(entrada3);
+            
+                            let funcion1=math.parse(cadena1);
+                            let funcion2=math.parse(cadena2);
+                            let funcion3=math.parse(cadena3);
+            
+                            let derivada1=math.derivative(funcion1,"x").toString();
+                            console.log(derivada1)
+                            let derivada2=math.derivative(funcion2,"y").toString();
+                            console.log(derivada2)
+                            let derivada3=math.derivative(funcion3,"z").toString();
+                            console.log(derivada3)
+            
+                        let textoREsul="<h2>Divergencia</h2>";
+                        textoREsul += `(Ax): ${derivada1}<br>`;
+                        textoREsul += `(Ay): ${derivada2}<br>`;
+                        textoREsul += `(Az): ${derivada3}<br>`;
+            
+                        elementoResultado.innerHTML=textoREsul;
+                        
+            
+                
+             })
+    );
+}
+
+export default Div;
--- client/src/pages/Div.jsx
+++ client/src/pages/Div.jsx
@@ -1,7 +1,46 @@
+import div from '../operation/div';
 
 function Div() {
+   
+
   return (
-    <div>WELCOME DIVERGENGE</div>
+    <>
+     
+     <h2> WELLCOME DEV CALCULATED</h2>
+     
+
+     <section className='h-[calc(100vh-100px)]  px-4 py-2 rounded-md border-double flex items-center justify-center bg-black '>
+
+     <form action="" className=''>
+
+        <input  type="text" placeholder='Enter X' 
+        className='m-8 bg-violet-500	 placeholder:text-white		 text-black px-4 py-2 rounded-md' 
+        id='prima' />
+
+        <input  type="text" placeholder='Enter Y' 
+        className='m-8 bg-violet-500	 placeholder:text-white		 text-black px-4 py-2 rounded-md' 
+        id='secundaria' />
+
+        <input  type="text" placeholder='Enter Z' 
+        className='m-8 bg-violet-500	 placeholder:text-white		 text-black px-4 py-2 rounded-md' 
+        id='terciaria' />
+
+
+
+        
+        <button className="rounded-md p-2 m-8 bg-white text text-black">CALCULATED</button>
+
+    
+
+
+        </form>
+
+     </section>
+
+
+
+
+    </>
   )
 }
 
